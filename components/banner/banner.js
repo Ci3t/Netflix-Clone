140e6717012075afae39330e9b84f088628ba080
Ci3t <ranigfx@gmail.com>
2023-02-25 19:17:01 +0200

    starting

---
+++ components/banner/banner.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import styles from "../../styles/banner.module.css";
+function banner() {
+  return <div>banner</div>;
+}
+
+export default banner;
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,300;0,400;1,100&family=Bree+Serif&family=Comforter+Brush&family=Indie+Flower&family=Merienda+One&family=Montserrat+Subrayada:wght@400;700&family=Poppins:ital,wght@0,600;1,100;1,200&family=Quicksand:wght@500;700&family=Roboto+Slab&display=swap" rel="stylesheet"/>
       <body>
         <Main />
         <NextScript />
